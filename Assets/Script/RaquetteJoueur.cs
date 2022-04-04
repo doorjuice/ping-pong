e45ffe6290ffdc922f5d176c5e8c9f40d048f1fd
David Giasson <david.giasson@outlook.com>
2022-04-04 16:23:53 -0400

    Renamed classes and messing with table physics.

--- Assets/Script/RaquetteJoueur.cs
+++ Assets/Script/RaquetteJoueur.cs
@@ -2,24 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Raquette : MonoBehaviour
+public class RaquetteJoueur : MonoBehaviour
 {
 
     // Update is called once per frame
     void Update()
     {
         float deplacement = Input.GetAxis("Horizontal");
-        transform.Translate(deplacement * Time.deltaTime, 0, 0);
+        transform.Translate(0, 0, deplacement * 0.1f * Time.deltaTime);
 
         var posi = transform.position;
         posi.x = Mathf.Clamp(posi.x, -1.5f, 1.5f);
         transform.position = posi;
-            
-
 
     }
-
-
 
 
 }
