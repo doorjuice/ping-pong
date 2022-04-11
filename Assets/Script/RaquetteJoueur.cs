33b679e109a79c1b839eed2990af393dacde7450
David G <david.giasson@claurendeau.qc.ca>
2022-04-11 16:33:32 -0400

    Ajout du rayon pour positionner la raquette autour de la sphère de jeu. (WIP)

--- Assets/Script/RaquetteJoueur.cs
+++ Assets/Script/RaquetteJoueur.cs
@@ -38,6 +38,10 @@ public class RaquetteJoueur : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
-            Debug.Log(hit.point);
+        {
+            //Debug.Log(hit.collider.name); //Display the name of the collider hit
+            //Debug.Log(hit.point);
+            transform.position = hit.point;
+        }
     }
 }
